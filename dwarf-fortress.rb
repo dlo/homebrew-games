cd8cbcba3d72e93627e82dcee360b6448be20024
Dan Loewenherz <dlo@users.noreply.github.com>
2016-05-19 22:17:36 -0500

    dwarf-fortress 0.43.02

--- dwarf-fortress.rb
+++ dwarf-fortress.rb
@@ -1,16 +1,9 @@
 class DwarfFortress < Formula
   desc "Open-ended rogelike game"
   homepage "http://bay12games.com/dwarves/"
-  url "http://www.bay12games.com/dwarves/df_43_01_osx.tar.bz2"
-  version "0.43.01"
-  sha256 "7fe378b7aeee67f10a1f88a2341b8724edbf91795fd928506f01dfb403304d43"
-
-  bottle do
-    cellar :any
-    sha256 "8e818bce8b233273510e6f1131d5e5c9c3c1c44db3d9e5ea8ce0dad89ce773d2" => :el_capitan
-    sha256 "20811d2204053c0bbd67db4bcf52e39edc637cf5b3c9d8a6e6c789f6a0842350" => :yosemite
-    sha256 "a3dbfaa17c91330cb8e502bf2a9874d0f53682fd3c5140a2cb1716504c135c82" => :mavericks
-  end
+  url "http://www.bay12games.com/dwarves/df_43_02_osx.tar.bz2"
+  version "0.43.02"
+  sha256 "e4c98c42c81efc604cd73dd4b8361e06b511756acc4f6c88fe0d4919a6272368"
 
   depends_on :arch => :intel
   depends_on :x11
